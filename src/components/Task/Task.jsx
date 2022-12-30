78b5fe290ed047b5336458f99dcefea654d7f458
mpsfind <mpsfind@gmail.com>
2022-12-30 12:27:42 +0300

    initial

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -6,6 +6,10 @@ const Task = () => {
             <div className="task__id">1</div>
             <div className="task__header">My first task</div>
             {/* <div className="task__description">I will create the best app in the world</div> */}
+            
+            <div className="task__control">
+                <div className="task__success"></div>
+            </div>
         </div>
     )
 }
