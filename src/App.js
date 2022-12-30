ebfebda28ab01a42e0dbf29c9156bb99345090a9
mpsfind <mpsfind@gmail.com>
2022-12-30 11:39:39 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,58 +1,12 @@
 import { useEffect } from 'react';
 import './App.css';
+import Task from './components/Task/Task';
+import './components/Task/style.css';
 const tg = window.Telegram.WebApp;
 
 function App() {
-
-  useEffect(() => {
-      tg.ready()
-    }, [])
-
-  const onClose = () => {
-    tg.close()
-  }
-
   return (
-    <div className="App">
-
-      ЭТО НОВОЕ АПИ ДЕТКА
-      <br/>
-      ЕБАШУ НА РЕАКТЕ В ТЕЛЕГЕttttttttttttttasdasdasdddddddddddddddtttttt
-      <br/>
-      test
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      asdasdasd
-      <br/>
-      <br/>
-      <br/>
-      asdasdasd
-      asdasdasd
-      asdasdasd
-      <br/>
-      asdasdasd
-      asdasdasd
-      asdasdasd
-      <br/>
-      asdasdasd
-      asdasdasd
-      asdasdasd
-      <br/>
-      asdasdasd
-      asdasdasd
-      asdasdasd
-      <br/>
-      asdasdasd
-      asdasdasd
-      asdasdasd
-      <br/>
-      asdasdasd
-      asdasdasd
-      asdasdasd
-      <button onClick={onClose}>Close</button>
-    </div>
+    <Task/>
   );
 }
 
---
+++ src/components/Task/Task.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Task = () => {
+    return (
+        <div className="task">
+            <div className="task__id">1</div>
+            <div className="task__header">My first task</div>
+            {/* <div className="task__description">I will create the best app in the world</div> */}
+        </div>
+    )
+}
+
+export default Task
