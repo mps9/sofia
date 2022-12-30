b1e4742e9a6742aed9390a45d2e75bc70778406e
mpsfind <mpsfind@gmail.com>
2022-12-30 13:19:30 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -8,25 +8,25 @@ function App() {
   return (
     <div className='App'>
       <div className='header'>Today</div>
-      <Task/>
-      <Task/>
-      <Task/>
+      <Task info={{header: 'My first task', date: '30 december 12:30'}}/>
+      <Task info={{header: 'I need some sleep', date: '30 december 18:30'}}/>
+      <Task info={{header: 'I want to sleep', date: '30 december 18:30'}}/>
       <div className='header'>Yesterday</div>
-      <Task/>
-      <Task/>
-      <Task/>
-      <Task/>
-      <Task/>
-      <Task/>
-      <Task/>
-      <Task/>
-      <Task/>
-      <Task/>
-      <Task/>
-      <Task/>
-      <Task/>
-      <Task/>
-      <Task/>
+      <Task info={{header: 'Heppy new year', date: '31 december 23:59'}}/>
+      <Task info={{header: 'Heppy new year', date: '31 december 23:59'}}/>
+      <Task info={{header: 'Heppy new year', date: '31 december 23:59'}}/>
+      <Task info={{header: 'Heppy new year', date: '31 december 23:59'}}/>
+      <Task info={{header: 'Heppy new year', date: '31 december 23:59'}}/>
+      <Task info={{header: 'Heppy new year', date: '31 december 23:59'}}/>
+      <Task info={{header: 'Heppy new year', date: '31 december 23:59'}}/>
+      <Task info={{header: 'Heppy new year', date: '31 december 23:59'}}/>
+      <Task info={{header: 'Heppy new year', date: '31 december 23:59'}}/>
+      <Task info={{header: 'Heppy new year', date: '31 december 23:59'}}/>
+      <Task info={{header: 'Heppy new year', date: '31 december 23:59'}}/>
+      <Task info={{header: 'Heppy new year', date: '31 december 23:59'}}/>
+      <Task info={{header: 'Heppy new year', date: '31 december 23:59'}}/>
+      <Task info={{header: 'Heppy new year', date: '31 december 23:59'}}/>
+      <Task info={{header: 'Heppy new year', date: '31 december 23:59'}}/>
     </div>
   );
 }
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,11 +1,11 @@
 import React from "react"
 
-const Task = () => {
+const Task = (data) => {
     return (
         <div className="task">
             <div className="task__main">
-                <div className="task__main_header">My first task</div>
-                <div className="task__main_date">31 december 23:30</div>
+                <div className="task__main_header">{data.info.header}</div>
+                <div className="task__main_date">{data.info.date}</div>
             </div>
             {/* <div className="task__id">1</div> */}
             {/* <div className="task__description">I will create the best app in the world</div> */}
