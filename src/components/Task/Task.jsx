af9c69bac4a50201af43dde0e9a78141f8f04e2c
mpsfind <mpsfind@gmail.com>
2022-12-30 13:04:50 +0300

    initial

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -3,8 +3,11 @@ import React from "react"
 const Task = () => {
     return (
         <div className="task">
-            <div className="task__id">1</div>
-            <div className="task__header">My first task</div>
+            <div className="task__main">
+                <div className="task__main_header">My first task</div>
+                <div className="task__main_date">31 december</div>
+            </div>
+            {/* <div className="task__id">1</div> */}
             {/* <div className="task__description">I will create the best app in the world</div> */}
             
             <div className="task__control">
